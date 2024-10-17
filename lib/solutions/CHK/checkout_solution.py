91c6b2fc740b38554a0590c8fbb611c123a97321
sbx_user1051 <sbx_user1051@169.254.69.61>
2024-10-17 10:39:33 +0000

    Thu Oct 17 10:39:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,5 +11,16 @@ def checkout(skus: str) -> int:
     if not set(prices).issubset(('A', 'B', 'C', 'D')):
         return -1
     
-    return 0
+    cost = 0
+
+    if 'A' in prices:
+        cost += (prices['A']//3) * 130 + (prices['A']%3) * 50
+    if 'B' in prices:
+        cost += (prices['B']//2) * 45 + (prices['B']%2) * 30
+    if 'C' in prices:
+        cost += prices['C'] * 20
+    if 'D' in prices:
+        cost += prices['D'] * 15
+
+    return cost
 
