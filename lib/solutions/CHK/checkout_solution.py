81b92c6baaf7f0e31484d54c9fdfe8c4dd8ebe0b
sbx_user1051 <sbx_user1051@169.254.69.61>
2024-10-17 11:23:06 +0000

    Thu Oct 17 11:23:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,86 +23,148 @@ def checkout(skus: str) -> int:
         cost += (prices['A']//3) * 130
         prices['A'] %= 3
         cost += prices['A'] * 50
+
+        del prices['A']
+
     # E before B
     if 'E' in prices:
         prices['B'] = prices.get('B', 0) - prices['E']//2
         if prices['B'] <= 0:
             del prices['B']
         cost += prices['E'] * 40
+
+        del prices['E']
+
     if 'B' in prices:
         cost += (prices['B']//2) * 45
         prices['B'] %= 2
         cost += prices['B'] * 30
+
+        del prices['B']
+
     if 'C' in prices:
         cost += prices['C'] * 20
+
+        del prices['C']
+
     if 'D' in prices:
         cost += prices['D'] * 15
+
+        del prices['D']
+
     if 'F' in prices:
         cost += (prices['F']//3) * 20
         cost += (prices['F']%3) * 10
+
+        del prices['F']
+
     if 'G' in prices:
         cost += prices['G'] * 20
+
+        del prices['G']
+
     if 'H' in prices:
         cost += (prices['H']//10) * 80
         prices['H'] %= 10
         cost += (prices['H']//5) * 45
         prices['H'] %= 5
         cost += prices['H'] * 10
+
+        del prices['H']
+
     if 'I' in prices:
         cost += prices['I'] * 35
+
+        del prices['I']
+
     if 'J' in prices:
         cost += prices['J'] * 60
+
+        del prices['J']
+
     if 'K' in prices:
         cost += (prices['K']//2) * 120
         prices['K'] %= 2
         cost += prices['K'] * 70
+
+        del prices['K']
+
     if 'L' in prices:
         cost += prices['L'] * 90
+
+        del prices['L']
+        
     # N before M
     if 'N' in prices:
         prices['M'] = prices.get('M', 0) - prices['N']//3
         if prices['M'] <= 0:
             del prices['M']
         cost += prices['N'] * 40
+
+        del prices['N']
+
     if 'M' in prices:
         cost += prices['M'] * 15
+
+        del prices['M']
+
     if 'O' in prices:
         cost += prices['O'] * 10
+
+        del prices['O']
+
     if 'P' in prices:
         cost += (prices['P']//5) * 200
         prices['P'] %= 5
         cost += prices['P'] * 50
+
+        del prices['P']
+
     # R before Q
     if 'R' in prices:
         prices['Q'] = prices.get('Q', 0) - prices['R']//3
         if prices['Q'] <= 0:
             del prices['Q']
         cost += prices['R'] * 50
+
+        del prices['R']
+
     if 'Q' in prices:
         cost += (prices['Q']//3) * 80
         prices['Q'] %= 3
         cost += prices['Q'] * 30
+
+        del prices['Q']
+
     if 'U' in prices:
         cost += (prices['U']//4) * 120
         prices['U'] %= 4
         cost += prices['U'] * 40
+
+        del prices['U']
+
     if 'V' in prices:
         cost += (prices['V']//3) * 130
         prices['V'] %= 3
         cost += (prices['V']//2) * 90
         prices['V'] %= 2
         cost += prices['V'] * 50
+
+        del prices['V']
+
     if 'W' in prices:
         cost += prices['W'] * 20
 
+        del prices['W']
+
     # group discount
 
-    while len(discount_group) >= 3:
+    while len(prices) >= 3:
         cost += 45
-        for item in sorted(discount_group)[:3]:
-            discount_group[item] -= 1
-            if discount_group[item] == 0:
-                del discount_group[item]
+        for item in list(prices)[:3]:
+            prices[item] -= 1
+            if prices[item] == 0:
+                del prices[item]
         
     if 'S' in prices:
         cost += prices['S'] * 20
